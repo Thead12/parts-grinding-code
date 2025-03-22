ece2963a9fb979a59487a67024566cb252627cb4
WheatAutarky <101641009+WheatAutarky@users.noreply.github.com>
2025-03-22 16:23:22 +0000

    Update network test.py
    Updated to stop pinging if the counting script begins

--- network test.py
+++ network test.py
@@ -1,13 +1,14 @@
 from icmplib import multiping
 from subprocess import call
 
-while True:
+counting_is_running = False
+while counting_is_running is False:
     hosts = multiping(['158.125.93.1', '158.125.93.202'], interval=5)
     for host in hosts:
         if host.is_alive:
          print(f'{host.address} is up!')
          #pingtest.printtest()
          call(["python", "Downloads\pingtest.py"]) #specify full path of file - note for linux, path separators are '/' not '\'
+         counting_is_running = True
         else:
          print(f'{host.address} is down!')
-
